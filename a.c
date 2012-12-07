f34df1b6d4bdba066ad6104fa71d406c3df05e34
ongjohn <ongjohn@163.com>
2012-12-07 14:39:49 +0800

    commit last by local master

--- a.c
+++ a.c
@@ -2,9 +2,5 @@
 
 int main(void)
 {
-  printf("Hello,a.c on branch master!\r\n");
-  printf("Hello,a.c on branch master!\r\n");
-  printf("Hello,a.c on branch master!\r\n");
-  printf("Hello,a.c on branch master!\r\n");
   return;
 }
